a38c55b2812b0e72407d070dedd3e964a400bfcd
shams-786 <116788785+shams-786@users.noreply.github.com>
2024-09-29 14:09:24 +0530

    Revert "commit"
    This reverts commit 40e7685b7d1f4a1c9af16e9ddc615bd92690cbb6.

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.header("Learning is fun!!!!")
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
